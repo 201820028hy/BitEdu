5df95968cfb53bfd999f2f70035a2bf989eb9a96
yooni <yooni@192.168.0.145>
2023-05-22 21:07:20 +0900

    3일차 숫자 야구 게임 실

---
+++ 20230522_day03/src/ballGame/Client.java
@@ -0,0 +1,50 @@
+package ballGame;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Client {
+	private static boolean flag = true;
+   
+   public int[] play () {
+      Scanner sc = new Scanner(System.in);
+      
+      String input = sc.next();
+      if(input.contains("기권")) {
+    	  flag = false;
+    	  return null;
+    	  
+      }
+      int[] userBalls = new int[4];
+      List<Integer> intgers = new ArrayList<>();
+      char[] charArr = input.toCharArray();
+      
+      if(charArr.length < 4) {
+    	  System.out.println("4자리 숫자를 입력하세요.");
+    	  return null;
+      }
+      
+      for(int i = 0; i < charArr.length; i++) {
+    	 for(int j = 0; j < intgers.size(); j++) {
+    		 int charNum = Integer.parseInt(String.valueOf(charArr[i]));
+    		 if(intgers.contains(charNum)) {
+    	    	 System.out.println("중복되지 않은 숫자를 입력하세요.");
+    			 return null;
+    		 }
+    		 intgers.add(charNum);
+//             userBalls[i] = charNum;
+             
+    	 }
+      }
+      for(int i = 0; i < intgers.size();i++) {
+    	  userBalls[i] = (int) intgers.toArray()[i];
+      }
+
+      return userBalls;
+      
+   }
+   public boolean isFlag() {
+	   return this.flag;
+   }
+}
